1e5058f0529e33761b3750cc706815f69f9c0d17
poosapsap <badgevfi@gmail.com>
2025-05-22 20:09:59 -0400

    fix: configure CORS to allow requests from trenchmatch.com

--- trenchi-backend/app.js
+++ trenchi-backend/app.js
@@ -13,7 +13,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://trenchmatch.com', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
